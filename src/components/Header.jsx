d42d458ec120da84318a473c52ae4d539fe16f2c
Steven <onojasteven13@gmail.com>
2024-08-13 21:21:57 +0100

    added clerk signin and signup

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg">
+        <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
+            {/* logo */}
+            <Link href='/' className="text-2xl font-extrabold group cursor-pointer">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600">Auth</span>
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600">App</span>
+            </Link>
+            {/* navigation */}
+            <nav>
+                <ul className="flex gap-4">
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                    <SignedIn>
+                        <UserButton/>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton/>
+                    </SignedOut>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full border-t-4 border-red-500 border-solid h-20 w-20"></div>
+    </div>
+  )
+}
