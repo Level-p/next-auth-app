093e5a90d6fee79f4fe7999af2a33f742c9bac9a
Steven <onojasteven13@gmail.com>
2024-08-14 13:24:23 +0100

    jni

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -13,7 +13,7 @@ export const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "next-auth-app",
-            // useNewUrlParser: true,
+            useNewUrlParser: true,
             useUnifiedTopology: true,
         })
         console.log('MongoDB connected');
