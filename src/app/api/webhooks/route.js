2ce88bc4114de88a0cdbd0fec620151b5ab46cd3
Steven <onojasteven13@gmail.com>
2024-08-14 13:36:22 +0100

    work

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,93 +1,96 @@
-import { Webhook } from "svix"
-import { headers } from "next/headers"
-import { createOrUpdateUser, deleteUser } from "@/lib/actions/user"
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
 
-export default async function POST(req) {
+export async function POST(req) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-    const WEBHOOK_SECRET = process.env. WEBHOOK_SECRET
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+    );
+  }
 
-    if(!WEBHOOK_SECRET) {
-        throw new Error('Please add  WEBHOOK_SECRET from Clerk Dashboaerd to .env or .env.local')
-    }
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
-    // Get the headers
-    const headerPayload = headers()
-    const svix_id = headerPayload.get('svix-id')
-    const svix_timestamp = headerPayload.get('svix-tiemstamp')
-    const svix_signature = headerPayload.get('svix-signature')
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400,
+    });
+  }
 
-    // If there are no headers, error out
-    if(!svix_id || !svix_signature || !svix_timestamp) {
-        return new Response ('Error occured -- no svix headers', {
-            status: 400
-        })
-    }
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
-    // Get body 
-    const payload = await req.json()
-    const body = JSON.stringify(payload)
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-    // Create a new Svix instance with your secret.
-    const wh = new Webhook(WEBHOOK_SECRET);
+  let evt;
 
-    let evt
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    });
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400,
+    });
+  }
 
-    // Verify the payload with headers
+  // Do something with the payload
+  // For this guide, you simply log the payload to the console
+  const { id } = evt?.data;
+  const eventType = evt?.type;
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log('Webhook body:', body);
+
+  if (eventType === 'user.created' || eventType === 'user.updated') {
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
     try {
-        evt = wh.verify(body, {
-            "svix-id": svix_id,
-            "svix-timestamp": svix_timestamp,
-            "svix-signature":svix_signatiure
-        }) 
-    } catch (err) {
-        console.log('Error verifying webhooks:', err);
-        return new Response('Error occured', {
-            status: 400
-        })
+      await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+      return new Response('User is created or updated', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error creating or updating user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
     }
-
-    // Do something with the payload
-    // For this guide, you simply log the payload to the console
-    const { id } = evt.data
-    const eventType = evt.type
-    console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-    console.log('Webhook body:', body);
-
-    if(eventType === 'user.created' || eventType === "user.updated") {
-        const { id,  first_name, last_name, email_addresses, username} = evt.data
-        try {
-            await createOrUpdateUser ( 
-                id,
-                first_name,
-                last_name,
-                email_addresses,
-                username
-            )
-            return new Response('User is created or updated', {
-                status: 200
-            })
-        } catch (error) {
-            console.log('Error creating or updating user:', error);
-            return new Response('Error occured', {
-                status: 400
-            })
-        }
+  }
+  if (eventType === 'user.deleted') {
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+      return new Response('User is deleted', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error deleting user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
     }
+  }
 
-    if(eventType === 'user.deleted') {
-        const { id } = evt.data
-        try {
-            await deleteUser(id)
-            return new Response('User is deleted', {
-                status: 200
-            })
-        } catch (error) {
-            console.log('Error deleting user:', error);
-            return new Response('Error occured', {
-                status: 400
-            })
-        }
-    }
-
-  return new Response ('', {status: 200})
+  return new Response('', { status: 200 });
 }
